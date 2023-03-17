796b013b4d39144ee2fcd3296efaa952ceb75763
dev-rafa1707 <rafa1707@gmail.com>
2023-03-16 21:53:26 -0300

    add def insert

--- binaryTree.py
+++ binaryTree.py
@@ -5,3 +5,19 @@ class Node:
         self.data = data
         self.right = None
         self.left = None
+
+def insert(self, data):
+# Compare the new value with the parent node
+      if self.data:
+         if data < self.data:
+            if self.left is None:
+               self.left = Node(data)
+            else:
+               self.left.insert(data)
+         elif data > self.data:
+               if self.right is None:
+                  self.right = Node(data)
+               else:
+                  self.right.insert(data)
+      else:
+         self.data = data
