1542b08e1ba51b4f05826028a278de975ffa1df4
dev-rafa1707 <rafa1707@gmail.com>
2023-03-18 22:26:21 -0300

    add itens e testes

--- binaryTree.py
+++ binaryTree.py
@@ -1,76 +1,158 @@
 # Binary Tree
 
 class Node:
-   def __init__(self, data):
-      self.data = data
-      self.right = None
-      self.left = None
+   def __init__(self):
+      self._right = None
+      self._left = None
+      self._data = None
    
    @property
    def data(self):
-      return self.data
+      return self._data
    
    def __str__(self):
-      return str(self.data)
+      return str(self._data)
 
    
 class binaryTree:
    def __init__(self):
-      self.root = None
+      self._root = None
    
    def addNode(self,data):
       node = Node()
-      self.data = data
+      node._data = data
 
-      if self.data == None:
-         self.data = node
+      if self._root == None:
+         self._root = node
          return True
       else:
-         aux = self.root
+         aux = self._root
 
          while aux != None:
-            if data < aux.data:
+            if aux._data == data:
                return False
-            elif aux.data > data:
-               if aux.left != None:
-                  aux = aux.left
+            elif data < aux._data:
+               if aux._left != None:
+                  aux = aux._left
                else:
                   aux.left = node
                   return True
             else:
-               if aux.right != None:
-                  aux = aux.right
+               if aux._right != None:
+                  aux = aux._right
                else:
-                  aux.right = node
+                  aux._right = node
                   return True
+   
+
+
+   def inserir(self,valor):
+        nodo = Nodo()
+        nodo._valor = valor
+
+        if self._raiz == None:
+            self._raiz = nodo
+            return True
+        else:
+            aux = self._raiz
+
+            while aux != None:
+                if aux._valor == valor:
+                    print("Valor duplicado!")
+                    return False
+                elif valor < aux._valor:
+                    if aux._esquerda != None:
+                        aux = aux._esquerda
+                    else:
+                        aux._esquerda = nodo
+                        return True
+                else:
+                    if aux._direita != None:
+                        aux = aux._direita
+                    else:
+                        aux._direita = nodo
+                        return True
+
+
+
+
+
+
+
+
+
+
+
    def search(self, data):
-      aux = self.root
+      aux = self._root
       while self.data != data:
          if self.data < data:
-            aux = aux.left
+            aux = aux._left
          else:
-            aux = aux.right
+            aux = aux._right
          if self.data == None:
             return None
       return aux
          
 
-# Root, left, right
-def PreOrder(self, node):
-   if Node != None:
+   # Root, left, right
+   def preOrder(self, node):
+      if Node != None:
+         print(node._data)
+         self.preOrder(node._left)
+         self.preOrder(node._right)
+
+   #Left, root, right
+   def order(self, node):
+      if Node != Node:
+         self.order(node._left)
+         print(node._data)
+         self.order(node._right)
+
+   #Left, raight, root
+   def posOrder(self,node):
+      if Node != Node:
+         self.posOrder(node._left)
+         self.posOrder(node._right)
+         print(node._data)
+
+   def display(self,node,level):
+      count = level
+      for i in range(count):
+            print("--",end="")
+        
       print(node.data)
-      PreOrder(node.left)
-      PreOrder(node.right)
 
-#Left, root, right
-def Order(self, node):
-   if Node != Node:
-      Order(node.left)
-      print(node)
-      Order(node.right)
+      if node._left != None:
+            count += 1
+            self.display(node._left,count)
+        
+      if node._right != None:
+            self.display(node._right,count)
 
-def PosOrder(self,node):
-   if Node != Node:
-      PosOrder(node.left)
-      PosOrder(node.right)
-      print(node)
+
+
+
+
+
+arvore = binaryTree()
+
+
+
+
+arvore.addNode(53)
+arvore.addNode(30)
+arvore.addNode(72)
+arvore.addNode(14)
+arvore.addNode(39)
+arvore.addNode(9)
+arvore.addNode(23)
+arvore.addNode(34)
+arvore.addNode(49)
+arvore.addNode(61)
+arvore.addNode(84)
+arvore.addNode(79)
+arvore.display(arvore._root,0)
+
+print('Em ordem:')
+arvore.order(arvore._root)
